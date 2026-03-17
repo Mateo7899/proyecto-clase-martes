145e4dd29e4bd45bf5a34924e0dd24f934244de0
Samuel Redondo <sredondo400@unab.edu.co>
2026-03-16 22:20:56 -0500

    landing

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect()->route('productos.index');
+    return view('welcome');
 });
 
 Route::resource('productos', ProductController::class);
